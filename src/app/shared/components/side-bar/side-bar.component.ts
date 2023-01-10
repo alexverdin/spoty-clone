54c8e30a88c4bc35f376caeab452395619ca5cf6
alexverdin <navarrov.alex@gmail.com>
2023-01-10 16:39:27 -0600

    added icons on side-bar using ngOnInit

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -6,5 +6,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./side-bar.component.css']
 })
 export class SideBarComponent {
+  mainMenu: {
+    defaultOptions: Array<any>,
+    accessLink: Array<any>
+  } = {defaultOptions: [], accessLink: []}; // inicializamos el objeto de mainMenu
 
+  customOptions: Array<any> = [];
+
+  constructor() {};
+
+  ngOnInit(): void {
+    this.mainMenu.defaultOptions = [
+      {
+        name: 'Home',
+        icon: 'uil-estate',
+        router: ['/']
+      },
+      {
+        name: 'Search',
+        icon: 'uil-search',
+        router: ['/', 'history']
+      },
+      {
+        name: 'Library',
+        icon: 'uil-music',
+        router: ['/', 'favorites']
+      }
+    ];
+
+    this.mainMenu.accessLink = [
+      {
+        name: 'Crear Lista',
+        icon: 'uil-plus-square'
+      },
+      {
+        name: 'Canciones que te gustan',
+        icon: 'uil-heart-medical'
+      }
+    ];
+
+    this.customOptions = [
+      {
+        name: 'Mi lista N°1',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista N°2',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista N°3',
+        router: ['/']
+      }
+    ]
+  }
 }
